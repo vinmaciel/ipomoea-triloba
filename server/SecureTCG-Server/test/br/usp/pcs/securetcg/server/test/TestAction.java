420a7c19d15273e5c7265e5acda378896691aee1
vinmaciel <vinicius_ceth@yahoo.com.br>
2013-10-27 16:39:32 -0200

    Updated GenericDAO for CREATE and RETRIEVE. Hibernate now using annotations. Index now uses ActionForm. Updated sql script to auto increment id.

--- server/SecureTCG-Server/test/br/usp/pcs/securetcg/server/test/TestAction.java
+++ server/SecureTCG-Server/test/br/usp/pcs/securetcg/server/test/TestAction.java
@@ -1,6 +1,7 @@
 package br.usp.pcs.securetcg.server.test;
 
 import java.math.BigInteger;
+import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,14 +16,29 @@ public class TestAction extends Action {
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
 		TestActionForm taForm = (TestActionForm) form;
-		taForm.setMessage("Testing action form");
-		
-		TestClass1 obj = new TestClass1();
-		obj.setName("one_name");
-		obj.setData(new BigInteger("123456789").toByteArray());
+		String message = taForm.getMessage();
+		long id = taForm.getSearchId();
+		System.out.println("id: " + id + " message: " + message);
+
+		TestClass1DAO oneDAO = new TestClass1DAO();
+		TestClass2DAO twoDAO = new TestClass2DAO();
 		
 		try{
-			TestClass1DAO.add(obj);
+			TestClass1 one = new TestClass1();
+			one.setName(message);
+			one.setData(BigInteger.valueOf(Calendar.getInstance().getTimeInMillis()).toByteArray());
+			System.out.println("added one: " + oneDAO.add(one));
+			
+			one = oneDAO.get(taForm.getSearchId());
+			taForm.setMessage(new BigInteger(one.getData()).toString());
+			
+			if(id > 0) {
+				TestClass2 two = new TestClass2();
+				two.setDescription(message);
+				two.setTestClassOne(one);
+				
+				System.out.println("added two: " + twoDAO.add(two));
+			}
 		}
 		catch(Throwable t) {
 			taForm.setMessage(t.getMessage());
